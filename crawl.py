9b73ddba314615cb0b9bf98330b2ce2840078e4c
Jingyu Tang <113242468+jingyuhhh@users.noreply.github.com>
2024-07-28 17:19:33 +0800

    Add files via upload

--- crawl.py
+++ crawl.py
@@ -2,31 +2,46 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from collections import defaultdict
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options
+
+options = Options()
+options.add_argument("--headless")
 
 
 keywords = ["vr", "ar"]
 year_start = 2023
 year_end = 2024
+source = "IEEE" # "ACM" or "IEEE"
+
 data = defaultdict(dict)
 
 for keyword in keywords:
     for year in range(year_start, year_end + 1):
-        # IEEE
-        # url  =f"https://ieeexplore.ieee.org/search/searchresult.jsp?queryText={keyword}&highlight=true&returnFacets=ALL&returnType=SEARCH&matchPubs=true&ranges={year}_{year}_Year"
-        # ACM
-        url = f"https://dl.acm.org/action/doSearch?AllField={keyword}&expand=all&AfterYear={year}&BeforeYear={year}"
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36 Edg/126.0.0.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            'Referer': url,
-        }
+        if source == "IEEE":
+            url  =f"https://ieeexplore.ieee.org/search/searchresult.jsp?queryText={keyword}&highlight=true&returnFacets=ALL&returnType=SEARCH&matchPubs=true&ranges={year}_{year}_Year"
+            driver = webdriver.Edge()
+            driver.get(url)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            num = soup.find_all('span', class_='strong')
+            num = num[1].text
+            print(num)
+            
+        else: 
+            url = f"https://dl.acm.org/action/doSearch?AllField={keyword}&expand=all&AfterYear={year}&BeforeYear={year}"
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36 Edg/126.0.0.0',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+                'Referer': url,
+            }
+            
+            response = requests.get(url, headers=headers)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            num = soup.find('span', class_='hitsLength').text
+            
 
-
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        num = soup.find('span', class_='hitsLength').text
+        
         num = int(num.replace(',', ''))
         data[year][keyword] = num
 
